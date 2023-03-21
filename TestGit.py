5d1161619286831c08de3e619cf3f20afb350474
DivyamshuM <103683889+DivyamshuM@users.noreply.github.com>
2023-03-20 21:51:41 -0400

    Update TestGit.py

--- TestGit.py
+++ TestGit.py
@@ -1,16 +1,38 @@
+import datetime
+#import unittest
+#from Git import getData
+
+
+def my_brand(homework_name):
+    print("=*=*=*= Divyamshu Mandadi =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-A =*=*=*= ")
+    print("=*=*=*= The name of the homework assignment: {} =*=*=*= ".format(homework_name))
+    print("=*=*=*= Date and time: {} =*=*=*= ".format(datetime.datetime.now()))
+
+my_brand("Homework 04a - Develop with the Perspective of the Tester in mind")
 
 import unittest
+from unittest.mock import patch
 from Git import getData
+
 
 class TestGit(unittest.TestCase):
     
-    def test_getData(self):
+    @patch('Git.requests.get')
+    def test_getData(self, mock_requests):
+        mock_response = [{'commit': {'message': 'Initial commit'}}, 
+                         {'commit': {'message': 'Updated readme file'}}]
+        mock_requests.return_value.json.return_value = mock_response
+        
         result = getData("DivyamshuM", "Triangle567")
-        self.assertEqual(result, 19)
+        self.assertEqual(result, 2)
 
-    def test_getData(self):
-        result = getData("ahsgdkeh", "ahsjdgs")
-        self.assertNotEqual(result, 19)
+    @patch('Git.requests.get')
+    def test_getData_error(self, mock_requests):
+        mock_requests.side_effect = Exception('error')
+        with self.assertRaises(Exception):
+            getData("DivyamshuM", "Triangle567")
+
 
 if __name__ == '__main__':
     print('Running unit tests')
